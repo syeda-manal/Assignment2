b9fd46e3e4f086ae7fa6fdb2204692aa4d01e643
syeda-manal <>
2020-06-08 18:01:10 +0500

    Initial commit

---
+++ build/precache-manifest.360a52ab507063b103e16e8aaf07118b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4698bdb54a0b01319d20b6c7ff981166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92656341c25c4380d407",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92656341c25c4380d407",
+    "url": "/static/js/main.d394aa2e.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
